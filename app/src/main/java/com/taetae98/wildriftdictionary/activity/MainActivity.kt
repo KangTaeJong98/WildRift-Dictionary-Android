3d3ecfb01087d0948548e14a107f0f6c16d3440b
KangTaeJong98 <rkdxowhd98@naver.com>
2021-07-28 19:58:17 +0900

    DICTIONARY-2 MainActivity Navigation 구성

---
+++ app/src/main/java/com/taetae98/wildriftdictionary/activity/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.taetae98.wildriftdictionary.activity
+
+import android.os.Bundle
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.taetae98.wildriftdictionary.R
+import com.taetae98.wildriftdictionary.databinding.ActivityMainBinding
+import com.taetae98.wildriftdictionary.databinding.BindingActivity
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override val appBarConfiguration by lazy {
+        AppBarConfiguration(
+            setOf(
+                R.id.newsFragment, R.id.championFragment, R.id.itemFragment, R.id.runeFragment
+            )
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        onCreateSupportActionBar()
+        onCreateBottomNavigationView()
+    }
+
+    private fun onCreateSupportActionBar() {
+        setSupportActionBar(binding.toolbar)
+    }
+
+    private fun onCreateBottomNavigationView() {
+        with(binding.bottomNavigationView) {
+            setupWithNavController(navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/taetae98/wildriftdictionary/fragment/ItemFragment.kt
@@ -0,0 +1,8 @@
+package com.taetae98.wildriftdictionary.fragment
+
+import com.taetae98.wildriftdictionary.R
+import com.taetae98.wildriftdictionary.databinding.BindingFragment
+import com.taetae98.wildriftdictionary.databinding.FragmentItemBinding
+
+class ItemFragment : BindingFragment<FragmentItemBinding>(R.layout.fragment_item) {
+}
