584d3afe557c2a742d4a1844ec63268787e30d73
KangTaeJong98 <rkdxowhd98@naver.com>
2021-07-28 21:26:57 +0900

    DICTIONARY-4 WebViewFragment 완성

--- app/src/main/java/com/taetae98/wildriftdictionary/databinding/BindingAdapter.kt
+++ app/src/main/java/com/taetae98/wildriftdictionary/databinding/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.taetae98.wildriftdictionary.databinding
 
+import android.webkit.WebView
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
@@ -8,12 +9,23 @@ import com.bumptech.glide.Glide
 object BindingAdapter {
     @JvmStatic
     @BindingAdapter(value = ["imageURL", "errorImage"], requireAll = false)
-    fun setImage(view: ImageView, url: String?, @DrawableRes error: Int) {
+    fun setImageURL(view: ImageView, url: String?, @DrawableRes error: Int) {
         if (url == null) {
             view.setImageDrawable(null)
             return
         }
 
         Glide.with(view).load(url).error(error).into(view)
+    }
+
+    @JvmStatic
+    @BindingAdapter("url")
+    fun setURL(view: WebView, url: String?) {
+        if (url == null) {
+            view.loadDataWithBaseURL(null, "", null, null, null)
+            return
+        }
+
+        view.loadUrl(url)
     }
 }
